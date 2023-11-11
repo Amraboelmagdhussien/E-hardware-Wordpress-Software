67e54421e7cc1529e1e552c5ae1ebedf1e860bb1
AmrHussien <amraboelmagdhussien@gmail.com>
2023-11-11 17:49:13 +0200

    V1

---
+++ e-shop/wp-content/plugins/latest-post-shortcode/lps-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f7613a3061b303adbb3f');
---
+++ e-shop/wp-content/plugins/latest-post-shortcode/lps-block/build/pagination.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '02a7680058d9904ab5f3');
